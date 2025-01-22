1e4790eb887f2efbcab100ab35c8ed22b005c99a
Alicia <aliciakathleenp@gmail.com>
2025-01-22 18:42:56 -0300

    criação da página de cardápio

--- script.js
+++ script.js
@@ -32,37 +32,6 @@ class SimpleSlider {
   }
 }
 
-class ImageSlider {
-  constructor(selector) {
-    this.track = document.querySelector(selector);
-    this.slides = this.track.querySelectorAll(".slider-slide");
-    this.currentIndex = 0;
-    this.slideWidth = this.slides[0].clientWidth;
-    this.initSlider();
-  }
-
-  initSlider() {
-    const firstSlideClone = this.slides[0].cloneNode(true);
-    this.track.appendChild(firstSlideClone);
-
-    setInterval(() => this.nextSlide(), 3000);
-  }
-
-  nextSlide() {
-    this.currentIndex++;
-    this.track.style.transform = `translateX(-${this.currentIndex * 100}%)`;
-    this.track.style.transition = "transform 0.5s ease-in-out";
-
-    if (this.currentIndex >= this.slides.length) {
-      setTimeout(() => {
-        this.track.style.transition = "none";
-        this.currentIndex = 0;
-        this.track.style.transform = "translateX(0)";
-      }, 500);
-    }
-  }
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   new SimpleSlider(".slider-wrapper");
   new ImageSlider(".slider-track");
@@ -102,3 +71,4 @@ document.querySelectorAll('.nav-link, .nav-logo').forEach(anchor => {
       });
   });
 });
+
